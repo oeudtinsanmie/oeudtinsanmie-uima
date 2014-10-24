55daccc26afa02aee4d89b1721a8cc3b2f575690
Douglas McClusky <drmcclus@ncsu.edu>
2014-10-24 16:32:15 -0400

    quick documentation.  Better supprt to be added later

--- README.md
+++ README.md
@@ -1,3 +1,19 @@
-# uima #
+UIMA
+====
 
-This is the uima module. It provides...
+ This module manages installation of UIMA and UIMA-AS from source, as well as installing the UIMA eclipse plugins
+
+Parameters:
+----------- 
+ * version
+   - The version of UIMA, UIMA-AS and eclipse plugins (other than ruta, which does not have versions all the way to the latest number)   
+ * ruta_version
+   - The version of the ruta eclipse plugin 
+ * revision
+   - If pulling the latest UIMA and UIMA-AS from trunk, the revision to pull.  If left undefined, pulls latest revision
+ * eclipsenv
+   - Eclipse release to install.  If set to undef, skips installation of eclipse
+ * eclipserel
+   - Service release of eclipse to install 
+ * install_prereqs
+   - Whether to install prerequisites for UIMA and UIMA-AS and set the JAVA_HOME environment variable
--- manifests/init.pp
+++ manifests/init.pp
@@ -1,10 +1,20 @@
 # Class: uima
 #
-# This module manages uima
+# This module manages installation of UIMA and UIMA-AS from source, as well as installing the UIMA eclipse plugins
 #
-# Parameters: none
-#
-# Actions:
+# Parameters: 
+# [*version*]
+#   - The version of UIMA, UIMA-AS and eclipse plugins (other than ruta, which does not have versions all the way to the latest number) 
+# [*ruta_version*]
+#   - The version of the ruta eclipse plugin 
+# [*revision*]
+#   - If pulling the latest UIMA and UIMA-AS from trunk, the revision to pull.  If left undefined, pulls latest revision
+# [*eclipsenv*]
+#   - Eclipse release to install.  If set to undef, skips installation of eclipse
+# [*eclipserel*]
+#   - Service release of eclipse to install 
+# [*install_prereqs*]
+#   - Whether to install prerequisites for UIMA and UIMA-AS and set the JAVA_HOME environment variable
 #
 # Requires: see Modulefile
 #
